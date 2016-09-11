4d98f01efbf6f60f3c925437f8e025a768681b35
Adroso360 <adrian.lapico@my.jcu.edu.au>
2016-09-11 22:57:05 +1000

    Excersize 5

--- Excersizes/EX5/Element.java
+++ Excersizes/EX5/Element.java
@@ -3,5 +3,28 @@ package EX5;
 /**
  * Created by Adroso360 on 11/09/2016.
  */
-public class Element {
+public abstract class Element
+{
+    String symbol;
+    int atomicNumber;
+    double atomicWeight;
+    public Element(String s, int an, double aw)
+    {
+        symbol = s;
+        atomicNumber =an;
+        atomicWeight = aw;
+    }
+    public String getSymbol()
+    {
+        return symbol;
+    }
+    public int getAtomicNumber()
+    {
+        return atomicNumber;
+    }
+    public double getAtomicWeight()
+    {
+        return atomicWeight;
+    }
+    public abstract void describeElement();
 }
--- Excersizes/EX5/MetalElement.java
+++ Excersizes/EX5/MetalElement.java
@@ -3,5 +3,17 @@ package EX5;
 /**
  * Created by Adroso360 on 11/09/2016.
  */
-public class MetalElement {
+public class MetalElement extends Element
+{
+    public MetalElement(String s, int an, double aw)
+    {
+        super(s, an, aw);
+    }
+    public void describeElement()
+    {
+        System.out.println("A metal, like " + getSymbol() +
+                " with atomic number " + getAtomicNumber() +
+                " and an atomic weight of " + getAtomicWeight() +
+                "\n    is a good conductor of electricity");
+    }
 }
